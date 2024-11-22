f9562a0abdc6d1acd3d5fa9325f9d8e11f600601
marco91garcia <Gvs10marco@gmail.com>
2024-11-22 09:41:08 -0500

    Se realianz cambios de identación

--- views/home.php
+++ views/home.php
@@ -7,13 +7,14 @@ if (!isset($_SESSION["usu_id"])) {
     exit();
 }
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>AdminLTE 3 | Blank Page</title>
-
   <?php require_once("modulos/css.php"); ?>
 </head>
 <body class="hold-transition sidebar-mini">
